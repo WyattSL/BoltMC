7af7efc7ad7c1b037ae7fa8f5ce058469786c231
Wyatt Lipscomb <35582368+WyattSL@users.noreply.github.com>
2020-07-01 17:28:00 -0400

    Command Handler; +db

--- index.js
+++ index.js
@@ -5,4 +5,48 @@ client.on("ready", () => {
   console.log("Bot Ready")
 });
 
+function c_db(msg, args) {
+  var code = args.join(" ")
+  var x = "```"
+  msg.channel.send(`${x}sql\n${code}${x}`);
+  db.run(code, function(err) {
+    var x = "``"
+    if (err) {
+      msg.channel.send(`Execution error: ${x}\nfix${err}${x}`)
+    } else {
+      msg.channel.send(`Success.`)
+    }
+  });
+}
+
+this.db.function = c_db
+this.db.usage = "+db <code>"
+this.db.owneronly = true
+this.db.description = "Executes SQLite3."
+
+client.on("message", (msg) => {
+  if (msg.author.bot) return
+  if (!msg.content.startsWith("+")) return;
+  var args = msg.content.trim().split(" ")
+  var cmd = args.shift().lowercase()
+  if (this[cmd]) {
+    msg.delete();
+    if (this[cmd].owneronly && msg.author.id != 270035320894914560) {
+      msg.channel.send("You do not have access to this command!")
+      return
+    }
+    if (this[cmd].permission && this[cmd].permission[0]) {
+      var i;
+      for (i=0;i<this[cmd].permission.length;i++) {
+        var p = this[cmd].permission[i]
+        if (!msg.member.hasPermission(p, false, true, true)) {
+          msg.channel.send("You do not have permission to use this command.")
+          return;
+        }
+      }
+    }
+    this[cmd].function(msg, args);
+  }
+});
+
 client.login(require("./TOKEN.json").TOKEN)
