ed0a930fb16a6ff9f502afbbd9b1d330ee2f150b
Wyatt Lipscomb <35582368+WyattSL@users.noreply.github.com>
2020-07-01 22:38:58 -0400

    almost

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ client.on("message", (msg) => {
 
 function updateStatus(id, name) {
   if (name == "BungeeCord") name = "Proxy"
-  var channel = client.guilds.first().channels.find(ch => ch && ch.name && ch.name.split(" ")[1].split(" ")[0] == name)
+  var channel = client.guilds.first().channels.find(ch => ch && ch.name && ch.name.split(" ")[1] && ch.name.split(" ")[1].split(" ")[0] == name)
   panel.getServerStatus(id).then(status => {
     if (status == "on") {
       channel.setName(`🟢 ${name} 🟢`)
